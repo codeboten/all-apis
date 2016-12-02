f1b759f8f4dc206b0c3e02d51a4d61c046b107df
Alex Ianchici <rawr@alrex.ca>
2016-12-02 06:42:31 -0800

    cleanup

--- netapi/app/main.go
+++ netapi/app/main.go
@@ -1,34 +1,3 @@
-//
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"io/ioutil"
-// )
-//
-// // Rankings struct
-// type Rankings struct {
-// 	Keyword  string
-// 	GetCount uint32
-// 	Engine   string
-// 	Locale   string
-// 	Mobile   bool
-// }
-//
-// func main() {
-// 	var jsonBlob = []byte(`
-//     {"keyword":"hipaa compliance form", "get_count":157, "engine":"google", "locale":"en-us", "mobile":false}
-//   `)
-// 	rankings := Rankings{}
-// 	err := json.Unmarshal(jsonBlob, &rankings)
-// 	if err != nil {
-// 		// nozzle.printError("opening config file", err.Error())
-// 	}
-//
-// 	rankingsJSON, _ := json.Marshal(rankings)
-// 	err = ioutil.WriteFile("output.json", rankingsJSON, 0644)
-// 	fmt.Printf("%+v", rankings)
-// }
-
 package main
 
 import (
